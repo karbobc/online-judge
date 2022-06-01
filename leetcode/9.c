d82b676d572bf484dd1a68c0a6526531a439b8f3
Karbob <karbobc@gmail.com>
2022-06-01 20:22:38 +0800

    S = O(1), T = O(n)

--- leetcode/9.c
+++ leetcode/9.c
@@ -10,28 +10,12 @@ bool isPalindrome(const int x) {
     if (x < 0) {
         return false;
     }
-    int n = x;
-    int len = 0;
+    long n = x, m = 0;
     while (n > 0) {
-        len++;
+        m = m*10 + n%10;
         n /= 10;
     }
-    int* arr = (int *) calloc(len, sizeof(int));
-    n = x;
-    // copy to array
-    for (int i = 0; i < len; i++) {
-        arr[i] = n % 10;
-        n /= 10;
-    }
-    // check
-    for (int i = 0; i < len >> 1; i++) {
-        if (arr[i] != arr[len-i-1]) {
-            free(arr);
-            return false;
-        }
-    }
-    free(arr);
-    return true;
+    return m == x;
 }
 
 int main(int args, char* argv[]) {
