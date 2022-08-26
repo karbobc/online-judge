2320e98d608837df56641e76051d3c74c50e52de
Karbob <karbobc@gmail.com>
2022-08-26 14:44:53 +0800

    S = O(1), T = O(log n), n=x

--- leetcode/base/69.c
+++ leetcode/base/69.c
@@ -8,15 +8,17 @@
 #define debug(S, ...) fprintf(stderr, S"\n", ##__VA_ARGS__)
 
 int mySqrt(int x) {
+  if (x == 1) return x;
   int ret = 0;
-  for (long i = 0;;i++) {
-    if (i*i == x) {
-      ret = i;
-      break;
-    }
-    if (i*i > x) {
-      ret = i-1;
-      break;
+  int l = 0;
+  int r = x - 1;
+  while (l <= r) {
+    long mid = (l + r) >> 1;
+    if (mid*mid <= x) {
+      ret = mid;
+      l = mid + 1;
+    } else {
+      r = mid - 1;
     }
   }
   return ret;
